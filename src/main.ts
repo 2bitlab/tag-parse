b1193d9b4834d34a18558984733e14a934d9104e
Jin <439661734@qq.com>
2023-05-03 21:20:13 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,96 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {
+  formatTime,
+  getBranchByHead,
+  getBranchByTag,
+  getSyncBranch,
+  getTagUrl
+} from './utils'
+
+import axios from 'axios'
+// debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+const ref = github.context.ref
+const pushPayload: any = github.context.payload
+
+console.log('github-----', github)
+console.log('github.context', github.context)
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting11111111 ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const topRepository: string = core.getInput('repository')
+    const githubToken: string = core.getInput('githubToken')
+    const type: string = core.getInput('type')
+    const runCommond: string = core.getInput('runCommond')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    console.log('topRepository:', topRepository)
+    console.log('type:', type)
+    console.log('runCommond:', runCommond)
 
-    core.setOutput('time', new Date().toTimeString())
+    if (type === 'stringify') {
+      const branch = getBranchByHead(ref) || getBranchByTag(ref)
+      const {repository, pusher} = pushPayload || {}
+      const {full_name} = repository || {}
+      const {name: pusherName} = pusher || {}
+      const [, outRepository] = full_name.split('/')
+      const syncBranch = getSyncBranch(ref)
+
+      const tagUrl = getTagUrl(topRepository || full_name)
+      const timesTamp = formatTime(new Date(), '{yy}-{mm}-{dd}-{h}-{i}-{s}')
+
+      const tagName = `${outRepository}/${syncBranch}/${timesTamp}/${runCommond}`
+      // `release/${timesTamp}&branch=${branch}&syncBranch=${syncBranch}&repository=${outRepository}`
+      const tagMessage = {
+        branch,
+        syncBranch,
+        repository: outRepository,
+        pusherName,
+        runCommond
+      }
+      console.log('tagName: ', tagName)
+      const ret = await axios({
+        method: 'POST',
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+          'content-type': 'application/json',
+          Authorization: `Bearer ${githubToken}`
+        },
+        url: tagUrl,
+        data: {
+          tag_name: tagName,
+          body: JSON.stringify(tagMessage)
+        }
+      })
+      console.log('ret------: ', ret.data)
+    }
+    if (type === 'parse') {
+      const {release} = pushPayload || {}
+      const {body} = release || {}
+      const tagInfo: any = JSON.parse(body)
+      console.log('tagInfo: ', tagInfo)
+      const {
+        branch: tagBranch,
+        syncBranch: tagSyncBranch,
+        repository: tagRepository,
+        pusherName
+      } = tagInfo || {}
+      console.log('branch: ', tagSyncBranch)
+      console.log('syncBranch----', tagBranch)
+      console.log('repository----', tagRepository)
+      console.log('pusherName----', pusherName)
+      console.log('runCommond----', tagInfo.runCommond)
+
+      core.exportVariable('BRANCH', tagBranch)
+      core.exportVariable('syncBranch', tagSyncBranch)
+      core.exportVariable('REPOSITORY', tagRepository)
+      core.exportVariable('runCommond', tagInfo.runCommond)
+    }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    const e: any = error
+    core.setFailed(e.message)
   }
 }
-
 run()
